299a70d26e501029517556a3c361f7bbfe2ab600
Laura <Laura@DESKTOP-KPK2QGV>
2021-04-16 13:07:47 +0300

    initial commit

---
+++ src/flexing/Main.java
@@ -0,0 +1,40 @@
+package flexing;
+
+class Compressor{
+	void comp() {
+		
+	}
+	
+	
+	
+	void decomp() {
+		
+	}
+	
+	
+	
+	void size() {
+		
+	}
+	
+	
+	
+	void equal() {
+		
+	}
+	
+	
+	
+	void about() {
+		System.out.print("Info");
+	}
+}
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
